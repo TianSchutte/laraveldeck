9a6c68cd3cf44524eb089e4d8627c68c77ead6a4
TianSchutte <tian@giantprocurement.guru>
2023-02-14 07:39:41 +0200

    -code readability -abstraction of api calls -fixing of validation on some methods

---
+++ app/Http/Controllers/Api/ApiBaseController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+
+abstract class ApiBaseController extends Controller
+{
+    /**
+     * @param array $data
+     * @param int $status
+     * @return JsonResponse
+     */
+    protected function response(array $data, int $status = 200) : JsonResponse
+    {
+        return response()->json($data, $status);
+    }
+
+    /**
+     * @param string $message
+     * @param int $status
+     * @return JsonResponse
+     */
+    protected function errorResponse(Request $request, string $message, int $status = 400)
+    {
+        Log::error($message, [
+            'url' => $request->url(),
+            'header' => $request->header(),
+            'ip' => $request->ip(),
+        ]);
+        return response()->json(['error' => $message], $status);
+    }
+}
